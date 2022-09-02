d2cf1e8804a05b43d9510eb2b064c0bcf6f0a7cd
Solution-c <solution_@outlook.kr>
2022-09-02 13:29:28 +0900

    first commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int x; // 첫번째 정수
+    int y; // 두번째 정수
+    int sum; // 두 정수의 합
+
+    x = 100;
+    y = 200;
+
+    sum = x+y;
+
+    printf("두 수의 합 = %d \n %f", sum,x);
+    return 0;
+}
---
+++ max.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    10+20;
+    return 0;
+}

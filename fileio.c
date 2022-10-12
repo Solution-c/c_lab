18b688040f560ca137deed4646b58f0944adf120
Solution-c <solution_@outlook.kr>
2022-10-12 15:05:39 +0900

    complete file io

--- fileio.c
+++ fileio.c
@@ -6,40 +6,54 @@
 
 typedef struct book{
     int num;
-    char title[50];
-    char author[50];
+    char title[300];
+    char author[100];
     int ishere;
 }book;
 
 void filecheck(FILE *fp);
-bool isadded;
+void render();
+void select();
 void add();
 void load();
 void show(int num);
+int search();
+void borrow();
+void breturn();
+void repeat();
+
+int sel;
+bool isadded;
 bool isloaded;
 book lib[MAX_SIZE];
 
 int main() {
-    int sel;
-    do{
-        printf("----------------------------------\n");
-        printf("1. 책 번호로 검색\n");
-        printf("2. 책 정보 추가");
-        printf("3. 대출");
-        printf("----------------------------------\n");
-        scanf("%d", &sel);
-        }while(0<sel&&sel<5);
+    render();
     return 0;
 }
 
 void filecheck(FILE* fp){
     if (fp==NULL) {
-        printf("파일 로딩 실패");
+        printf("데이터 로딩 실패\n");
         exit(1);
     }
-    else printf("파일 로딩 성공");
+    else printf("데이터 로딩 성공\n");
 }
+void render(){
 
+    do{
+        printf("---------------------\n");
+        printf("1. 검색\n");
+        printf("2. 추가\n");
+        printf("3. 대출\n");
+        printf("4. 반납\n");
+        printf("5. 종료\n");
+        printf("---------------------\n");
+        printf("선택:");
+        scanf("%d", &sel);
+        }while(sel<1||sel>5);
+    select();
+}
 void load(){
     int n=0;   
     FILE *fp;
@@ -59,16 +73,37 @@ void load(){
     isadded = false;
 }
 
+void select(){
+    switch(sel){
+        case 1: 
+            search();
+            break;
+        case 2:
+            add();
+            break;
+        case 3:
+            borrow();
+            break;
+        case 4:
+            breturn();
+            break;
+        case 5:
+            exit(1);
+            break;
+    }
+    repeat();
+}
+
 void add(){
     FILE* fp;
-    int num;
-    char title[50];
-    char author[15];
+    int num=0;
+    char title[50]={'\0'};
+    char author[15]={'\0'};
     
     fp = fopen("list.txt", "a+");
     filecheck(fp);
-
-    printf("\n번호 : ");
+    printf("---------------------\n");
+    printf("번호 : ");
     scanf("%d", &num);
     getchar();
 
@@ -77,6 +112,7 @@ void add(){
     title[strlen(title)-1]='\0';
     printf("저자명 : ");
     fgets(author, 15*sizeof(char), stdin);
+    printf("---------------------\n");
     author[strlen(author)-1]='\0';
 
     fprintf(fp, "%04d, %s, %s, 1\n", num, title, author);
@@ -87,8 +123,58 @@ void add(){
 
 void show(int num){
     if(!isloaded || isadded) load();
+    printf("---------------------\n");
     printf("\n num : %04d", lib[num].num);
     printf("\n title : %s", lib[num].title);
     printf("\n author : %s", lib[num].author);
     printf("\n ishere : %d\n", lib[num].ishere);
+    printf("---------------------\n");
+}
+
+int search(){
+    int num;
+    char s;
+
+    printf("[검색] 책 번호:");
+    scanf("%d", &num);
+    getchar();
+    show(num);
+    return num;
+}
+
+void repeat(){
+    char c;
+    printf("계속합니까?(y/n):");
+    scanf("%c", &c);
+    getchar();
+    switch(c){
+        case 'y':
+            select();
+            break;
+        case 'n':
+            render();
+            break;
+        default :
+            repeat();
+            break;
+    }
+}
+
+void borrow(){
+    int num = search();
+    if(lib[num].ishere ==1){
+        lib[num].ishere = 0;
+        printf("%s 대출 성공\n", lib[num].title);
+        show(num);
+    }
+    else if(lib[num].ishere ==0){
+        printf("대출 불가능\n");
+    }
+}
+
+void breturn(){
+    int num = search();
+    lib[num].ishere = 1;
+    printf("%s 반납 성공\n", lib[num].title);
+    show(num);
 }
